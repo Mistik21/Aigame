aeed33383044f60d53e06e80882c3d6d093be135
filin121666 <mur.my.75@mail.ru>
2024-06-17 15:18:56 +0300

    change time zone

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+from django.http import HttpRequest, HttpResponse
 
-def front(request):
+
+def front(request: HttpRequest) -> HttpResponse:
     context = {}
     return render(request, "index.html", context)
